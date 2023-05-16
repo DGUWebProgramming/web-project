4b50e8af6a5d06deab9e8fb2501875e306f2f840
JZU0 <ljzh1030@gmail.com>
2023-05-17 00:39:22 +0900

    [#6] Main page 추가

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./index.css";
+
+const Main = () => {
+  return (
+    <>
+      <div>메인 페이지</div>
+    </>
+  );
+};
+
+export default Main;
