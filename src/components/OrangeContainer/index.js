c43f2ef5467a37747748869b9e1866f14061f851
JZU0 <ljzh1030@gmail.com>
2023-05-20 01:20:40 +0900

    [#11] feat : add orange background container component

---
+++ src/components/OrangeContainer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../asset/logo.png";
+import { Link } from "react-router-dom";
+
+import "./index.css";
+
+const OrangeContainer = ({ category, children }) => {
+  return (
+    <div className="orange">
+      <Link to="/">
+        <img className="logo" src={logo} alt="로고" />
+      </Link>
+      <div className="white">
+        <div className="category">{category}</div>
+        <div className="white-child">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default OrangeContainer;
