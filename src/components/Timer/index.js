79d6940000cc1a144170f0d0a7fc5c318d27d4b5
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-05-23 01:18:07 +0900

    [#13] feat : Timer(남은 시간) 컴포넌트 추가

---
+++ src/components/Timer/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+import "../Timer/index.css"
+
+const Timer = () => {
+  // difficulty
+
+  const [limitTime, setlimitTime] = useState(90);
+  //useState(difficulty === "상" ? 30 : 60);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setlimitTime(limitTime => limitTime - 1);
+    }, 1000);
+    if (limitTime === 0) {
+      clearInterval(id);
+    }
+    return () => clearInterval(id);
+  }, [limitTime]);
+
+  return (
+    <>
+      <div className="timeBox">
+        <div>남은 시간</div>
+        <div id="time_text">
+          <span>{Math.floor(limitTime / 60)}분</span>
+          <span>{limitTime % 60}초</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Timer;
