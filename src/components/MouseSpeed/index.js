cfc7202bc61db07f27c41df43ea647b56195e6d2
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-05-23 11:22:22 +0900

    [#12] feat : main창의 버튼 클릭시 속도가 측정되도록 수정

--- src/components/MouseSpeed/index.js
+++ src/components/MouseSpeed/index.js
@@ -3,23 +3,24 @@ import "./index.css"
 import mouse from "../../asset/Mouse.png"
 import { useState, useEffect } from 'react';
 
-const MouseSpeed = () => {
+const MouseSpeed = ({clickValue}) => {
 
   const [startTime, setStartTime] = useState(0);
   const [firstClickTime, setFirstClickTime] = useState(0);
-
-  const handleClick = () => {
-    if (firstClickTime === 0) {
-      const currentTime = Date.now();
-      const timeElapsed = currentTime - startTime;
-      setFirstClickTime(timeElapsed);
-    }
-  };
+  const [onoff, setONOFF] = useState(false);
 
   useEffect(() => {
     const loadingCompletedTime = Date.now();
     setStartTime(loadingCompletedTime);
   }, []);
+
+  useEffect(() => {
+    if (firstClickTime === 0 && clickValue) {
+      const currentTime = Date.now();
+      const timeElapsed = currentTime - startTime;
+      setFirstClickTime(timeElapsed);
+    }
+  }, [clickValue]);
 
   const formatTime = (time) => {
     return time > 0 ? `${time}  ` : '0';
@@ -27,7 +28,7 @@ const MouseSpeed = () => {
 
   return (
     <>
-      <div className="speedBox" onClick={handleClick}>
+      <div className="speedBox" >
         <img src={mouse} alt={"mouse_image"} />
         <div>
           <p>마우스 클릭 속도</p>
