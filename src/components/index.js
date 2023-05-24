46aaafa0881d8440c551c05404e1aa5de2b4db0f
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-05-24 20:07:17 +0900

    docs : Timer, MouseSpeed 컴포넌트 export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Text } from "./Text";
 export { default as OrangeContainer } from "./OrangeContainer";
+export { default as Timer } from "./Timer";
+export { default as MouseSpeed } from "./MouseSpeed";
