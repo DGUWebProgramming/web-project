8531fee6449ab831a1fbf56ae61cb8e6a38880e0
AhnDo0 <i0796075@gmail.com>
2023-05-24 17:39:19 +0900

    [#16] feat: InfoContainer 컴포넌트 추가

---
+++ src/components/InfoContainer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import buttonIcon from "../../asset/button_icon.png";
+
+import "./index.css";
+
+const InfoContainer = ({ imgsrc, title }) => {
+  return (
+    <div className="info-container">
+      <div className="image-container">
+        <img className="image" src={imgsrc} alt="공연이미지"/>
+      </div>
+      <div className="button-container">
+        <div className="icon-background">
+          <img className="icon" src={buttonIcon} alt="버튼아이콘" />
+        </div>
+        <div className="title">{title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoContainer;
