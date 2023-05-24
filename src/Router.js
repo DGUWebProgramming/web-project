b5061c3f9a6c31554d88ac09e6a627635000b76b
Ag-crane <eunhak0401@naver.com>
2023-05-24 18:34:09 +0900

    [#17] feat : Ticketing 페이지 생성

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
 import OnBoarding from "./pages/OnBoarding";
+import Ticketing from "./pages/Ticketing";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<OnBoarding />} />
         <Route path="main" element={<Main />} />
+        <Route path="Ticketing" element={<Ticketing />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Ticketing/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import { useRef } from 'react';
+
+const Ticketing = () => {
+
+  return (
+    <>
+      <h1>Ticketing Page</h1>
+    </>
+  );
+  };
+export default Ticketing;
