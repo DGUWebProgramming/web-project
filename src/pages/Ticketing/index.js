4c437c396a5564e166fd4b2583a9e58bec348915
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-05-24 19:56:50 +0900

    [#17] feat : 티켓팅 연습 페이지 구현

--- src/pages/Ticketing/index.js
+++ src/pages/Ticketing/index.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { OrangeContainer } from "../../components";
+import { Timer } from "../../components";
+import { MouseSpeed } from "../../components";
 import "./index.css";
-import { useRef } from 'react';
 
 const Ticketing = () => {
+  const [click, setClick] = useState(false);
+  const [downClick, setDownClick] = useState(0);
+  const [upClick, setUpClick] = useState(0);
 
+  const checkSeat = () => {
+    setClick(true);
+  }
+
+  const checkSeatDown = () => {
+    const currentTime = Date.now();
+    setDownClick(currentTime);
+  }
+  
+  const checkSeatUp = () => {
+    const currentTime = Date.now();
+    setUpClick(currentTime);
+  }
+  
+  useEffect(() => {
+    console.log(click);
+  }, [click]);
+
+  useEffect(() => {
+    console.log(downClick);
+  }, [downClick]);
+  
+  useEffect(() => {
+    console.log(upClick);
+  }, [upClick]);
+  
   return (
     <>
-      <h1>Ticketing Page</h1>
+      <OrangeContainer className="OrangeContainer" category={"티켓팅연습"}>
+        <div className="infor">
+          <Timer clickValue={click} />
+          <MouseSpeed onMouseDownClick={downClick} onMouseUpClick={upClick}/>
+        </div>
+        <button id="seat" onClick={checkSeat} onMouseDown={checkSeatDown} onMouseUp={checkSeatUp}>좌석 배치도</button>
+      </OrangeContainer>
     </>
   );
-  };
+};
+
 export default Ticketing;
