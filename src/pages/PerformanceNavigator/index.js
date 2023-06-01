9b269caab7d13ac2f9fd89ad1f859f92b84eaf57
JZU0 <ljzh1030@gmail.com>
2023-06-01 19:26:51 +0900

    [#34] feat : add PerformanceNavigator page

---
+++ src/pages/PerformanceNavigator/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { OrangeContainer } from "../../components";
+
+import "./index.css";
+
+const PerformanceNavigator = () => {
+  return (
+    <>
+      <OrangeContainer category={"공연 정보 알아보기"}></OrangeContainer>
+    </>
+  );
+};
+
+export default PerformanceNavigator;
