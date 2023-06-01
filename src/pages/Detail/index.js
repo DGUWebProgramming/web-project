1de339fc49e704f5ac71fda81ac0ee19d3e678e1
AhnDo0 <i0796075@gmail.com>
2023-06-01 18:58:37 +0900

    [#22] refactor: 코드 수정

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { parseString } from "xml2js";
 
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { DetailInfo, OrangeContainer } from "../../components";
 
