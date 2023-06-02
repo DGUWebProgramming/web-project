c4a9c807b515e9d51736db5177a04627a9749951
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-06-02 11:53:24 +0900

    [#36] feat : ResultModal 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,4 @@ export { default as Modal } from "./Modal";
 export { default as SeatGroup } from "./SeatGroup";
 export { default as DeleyTimer } from "./DeleyTimer";
 export { default as DetailInfo } from "./DetailInfo";
+export { default as ResultModal } from "./ResultModal";
