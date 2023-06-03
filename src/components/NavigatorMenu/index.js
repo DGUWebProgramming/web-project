c726c4d2bdeb796b473db81c63d2250fcbd4faa4
AhnDo0 <i0796075@gmail.com>
2023-06-04 00:24:29 +0900

    [#34] style: menu 아이콘 추가 및 스타일링

--- src/components/NavigatorMenu/index.js
+++ src/components/NavigatorMenu/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Text from "../Text";
 
 import "./index.css";
 
-const NavigatorMenu = ({ menu }) => {
+const NavigatorMenu = ({ menu, img }) => {
   return (
     <div className="menu-container">
-      <p calssName="menu-title">{menu}</p>
+      <Link to="">
+        <div className="menu-box">
+        <img className="menu-icon" src={img} alt={menu} />
+        <Text styleClass="text6">{menu}</Text>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/pages/PerformanceNavigator/index.js
+++ src/pages/PerformanceNavigator/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { OrangeContainer } from "../../components";
+import { OrangeContainer, NavigatorMenu } from "../../components";
 
+import musical from "../../asset/images/musical.png";
+import play from "../../asset/images/play_theater.png";
+import concert from "../../asset/images/concert.png";
 import "./index.css";
-import NavigatorMenu from "../../components/NavigatorMenu";
 
 const PerformanceNavigator = () => {
   return (
     <>
       <OrangeContainer>
         <div className="navigator-container">
-          <NavigatorMenu menu={"뮤지컬"} />
+          <NavigatorMenu menu={"뮤지컬"} img={musical} />
           <div className="deviding-line"></div>
-          <NavigatorMenu menu={"연극"} />
-          <div className="devidng-line"></div>
-          <NavigatorMenu menu={"콘서트"} />
+          <NavigatorMenu menu={"연극"} img={play} />
+          <div className="deviding-line"></div>
+          <NavigatorMenu menu={"콘서트"} img={concert} />
         </div>
       </OrangeContainer>
     </>
