7b435bcb97c98d2bde53673be47e170b293ee501
AhnDo0 <i0796075@gmail.com>
2023-06-04 00:46:35 +0900

    [#34] feat: menu 링크 연결

--- src/components/NavigatorMenu/index.js
+++ src/components/NavigatorMenu/index.js
@@ -6,9 +6,25 @@ import Text from "../Text";
 import "./index.css";
 
 const NavigatorMenu = ({ menu, img }) => {
+  // menu 값에 따라서 Link의 대상을 결정
+  let linkTo = "";
+  switch(menu) {
+    case "뮤지컬":
+      linkTo = "/pi/GGGA";
+      break;
+    case "연극":
+      linkTo = "/pi/AAAA";
+      break;
+    case "콘서트":
+      linkTo = "/pi/CCCD";
+      break;
+    default:
+      linkTo = "";
+  }
+
   return (
     <div className="menu-container">
-      <Link to="">
+      <Link to={linkTo}>
         <div className="menu-box">
         <img className="menu-icon" src={img} alt={menu} />
         <Text styleClass="text6">{menu}</Text>
