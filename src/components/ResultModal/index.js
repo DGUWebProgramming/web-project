c73eb409d1d3de0677547af6b31ec0a6a34ee435
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-06-03 13:55:29 +0900

    [#36] feat : x 버튼 기능 수정

--- src/components/ResultModal/index.js
+++ src/components/ResultModal/index.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./index.css";
 
 const ResultModal = ({ clickValue, disableValue }) => {
   let result;
+  const [isVisible, setIsVisible] = useState(true);
 
   if (clickValue) {
     result = "성공";
@@ -11,29 +13,35 @@ const ResultModal = ({ clickValue, disableValue }) => {
   else if (disableValue) {
     result = "실패";
   }
-  else{
+  else {
     result = "실패";
   }
-
+  
   const reset = () => {
     window.location.reload()
   }
 
+  const hideComponent = () => {
+    setIsVisible(false);
+  };
+
   return (
     <>
-      <div className="modalBox">
-        <p className="content1">결과 발표 <Link to="/"><button className="X_btn">X</button></Link></p>
-        <p className="content2">{result}하셨습니다
-        <br />결과 통계를 확인하시겠습니까?</p>
-        <div className="btnGroup">
-          <Link to="/overview">
-            <button className="modalBtn ok" >확인</button>
-          </Link>
-          <button className="modalBtn again" onClick={reset}>재도전</button>
+      {isVisible && (
+        <div className="modalBox">
+          <p className="content1">결과 발표 <button className="X_btn" onClick={hideComponent}>X</button></p>
+          <p className="content2">{result}하셨습니다
+            <br />결과 통계를 확인하시겠습니까?</p>
+          <div className="btnGroup">
+            <Link to="/overview"> 
+              <button className="modalBtn ok" >확인</button>
+            </Link>
+            <button className="modalBtn again" onClick={reset}>재도전</button>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
 
-export default ResultModal
+export default ResultModal;
