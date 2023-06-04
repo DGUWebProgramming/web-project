2abdb0fdb0a1bcd2a4f9114a01c9b6246193029b
AhnDo0 <i0796075@gmail.com>
2023-06-04 13:56:51 +0900

    [#38] fix: 상세정보 호출값이 공백인 경우 대체 문구 설정

--- src/components/DetailInfo/index.js
+++ src/components/DetailInfo/index.js
@@ -4,15 +4,22 @@ import "./index.css";
 
 const DetailInfo = ({ performance }) => {
   const splitDetails = (detailInfo) => {
+    //값이 없는 경우
+    if (!detailInfo[0].trim()) return <li>해당 정보 없음</li>;
+
     // ,를 기준으로 정보를 쪼개어 줄넘김하여 출력
     const dates = detailInfo[0].split(", ");
     return dates.map((date, index) => <li key={index}>{date.trim()}</li>);
   };
 
+  const checkIfNull = (value) => {
+    return value[0].trim().length > 0 ? value : "해당 정보 없음";
+  };
+
   return (
     <div className="detail-container">
-      <p className="detail-genre">{performance.genrenm}</p>
-      <h2 className="detail-title">{performance.prfnm}</h2>
+      <p className="detail-genre">{checkIfNull(performance.genrenm)}</p>
+      <h2 className="detail-title">{checkIfNull(performance.prfnm)}</h2>
       <div className="detail-title-line"></div>
       <div className="detail-contents">
         <img className="detail-img" src={performance.poster} alt="공연포스터" />
@@ -23,14 +30,15 @@ const DetailInfo = ({ performance }) => {
               <strong>공연기간</strong>
             </div>
             <div className="infoDesc">
-              {performance.prfpdfrom}~{performance.prfpdto}
+              {checkIfNull(performance.prfpdfrom)}~
+              {checkIfNull(performance.prfpdto)}
             </div>
           </li>
           <li>
             <div className="infoItem">
               <strong>공연장소</strong>
             </div>
-            <div className="infoDesc">{performance.fcltynm}</div>
+            <div className="infoDesc">{checkIfNull(performance.fcltynm)}</div>
           </li>
           <li>
             <div className="infoItem">
@@ -46,7 +54,7 @@ const DetailInfo = ({ performance }) => {
             <div className="infoItem">
               <strong>관람연령</strong>
             </div>
-            <div className="infoDesc">{performance.prfage}</div>
+            <div className="infoDesc">{checkIfNull(performance.prfage)}</div>
           </li>
           <li>
             <div className="infoItem">
@@ -62,19 +70,21 @@ const DetailInfo = ({ performance }) => {
             <div className="infoItem">
               <strong>출연진</strong>
             </div>
-            <div className="infoDesc">{performance.prfcast}</div>
+            <div className="infoDesc">{checkIfNull(performance.prfcast)}</div>
           </li>
           <li>
             <div className="infoItem">
               <strong>러닝타임</strong>
             </div>
-            <div className="infoDesc">{performance.prfruntime}</div>
+            <div className="infoDesc">
+              {checkIfNull(performance.prfruntime)}
+            </div>
           </li>
           <li>
             <div className="infoItem">
               <strong>공연상태</strong>
             </div>
-            <div className="infoDesc">{performance.prfstate}</div>
+            <div className="infoDesc">{checkIfNull(performance.prfstate)}</div>
           </li>
         </ul>
       </div>
