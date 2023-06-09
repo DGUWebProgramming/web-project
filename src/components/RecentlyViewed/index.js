b82d22b86ef658527248e28bd909d9201182742b
AhnDo0 <i0796075@gmail.com>
2023-06-09 19:22:29 +0900

    [#42] feat: MiniInfo컴포넌트 바깥에 Link태그 추가

--- src/components/RecentlyViewed/index.js
+++ src/components/RecentlyViewed/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import { MiniInfo } from "../index";
 import "./index.css";
-import MiniInfo from "../MiniInfo";
 
 //localStorage에서 최근 본 공연 일부 몇 개 가져오기
 
@@ -9,8 +10,10 @@ const RecentlyViewed = () => {
   return (
     <div className="recentlyViewed-container">
       <div className="rv-title">최근 본 공연 목록</div>
+      {/* <Link to={`/detail/공연id값`}> */}
       {/* MiniInfo의 performance에 localStorage에서의 특정 공연데이터 객체 넣어주시면 됩니다. */}
-      <MiniInfo performance=""></MiniInfo>
+        <MiniInfo performance=""></MiniInfo>
+      {/* </Link> */}
     </div>
   );
 };
--- src/pages/OverView/index.js
+++ src/pages/OverView/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import { clickCountByDateAtom } from "../../state";
