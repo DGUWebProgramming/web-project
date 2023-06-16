6900d8492af9abb91433139c1fcd71d2a132733d
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-06-16 14:30:58 +0900

    [#43] feat : 로컬 스토리지에서 최근 공연 목록 가져와 출력

--- src/components/RecentlyViewed/index.js
+++ src/components/RecentlyViewed/index.js
@@ -1,21 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import { MiniInfo } from "../index";
-import "./index.css";
+import { MiniInfo } from '../index'
+import './index.css'
 
 //localStorage에서 최근 본 공연 일부 몇 개 가져오기
 
 const RecentlyViewed = () => {
-  return (
-    <div className="recentlyViewed-container">
-      <div className="rv-title">최근 본 공연 목록</div>
-      {/* <Link to={`/detail/공연id값`}> */}
-      {/* MiniInfo의 performance에 localStorage에서의 특정 공연데이터 객체 넣어주시면 됩니다. */}
-        <MiniInfo performance=""></MiniInfo>
-      {/* </Link> */}
-    </div>
-  );
-};
 
-export default RecentlyViewed;
+  const visitedKeys = Object.keys(localStorage).filter(key =>
+    key.includes('performanceDetail-')
+  )
+  const visitedKeysId = visitedKeys.map(key => key.split('-')[1])
+  visitedKeys.reverse()
+  console.log(visitedKeys)
+  const recentId = visitedKeysId[visitedKeysId.length - 1] 
+  const visitedData = visitedKeys.map(key => JSON.parse(localStorage.getItem(key)).dbs.db)
+  
+  return (
+    <div className='recentlyViewed-container'>
+      <div className='rv-title'>최근 본 공연 목록</div>
+      {visitedData.map((data) => (
+        <Link to={`/detail/${recentId}`}>
+          <MiniInfo performance={data[0]}></MiniInfo>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default RecentlyViewed
