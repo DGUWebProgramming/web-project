4bbfccf0ae47c4d34beabdf9fd491754cc805a23
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-06-16 15:18:11 +0900

    [#43] feat : 출력 개수 4개로 제한

--- src/components/RecentlyViewed/index.js
+++ src/components/RecentlyViewed/index.js
@@ -7,24 +7,27 @@ import './index.css'
 //localStorage에서 최근 본 공연 일부 몇 개 가져오기
 
 const RecentlyViewed = () => {
-
   const visitedKeys = Object.keys(localStorage).filter(key =>
     key.includes('performanceDetail-')
   )
   const visitedKeysId = visitedKeys.map(key => key.split('-')[1])
   visitedKeys.reverse()
   console.log(visitedKeys)
-  const recentId = visitedKeysId[visitedKeysId.length - 1] 
-  const visitedData = visitedKeys.map(key => JSON.parse(localStorage.getItem(key)).dbs.db)
-  
+  const recentId = visitedKeysId[visitedKeysId.length - 1]
+  let visitedData = visitedKeys.map(
+    key => JSON.parse(localStorage.getItem(key)).dbs.db
+  )
+  visitedData = visitedData.slice(Math.max(visitedData.length - 4, 0))
   return (
     <div className='recentlyViewed-container'>
       <div className='rv-title'>최근 본 공연 목록</div>
-      {visitedData.map((data) => (
-        <Link to={`/detail/${recentId}`}>
-          <MiniInfo performance={data[0]}></MiniInfo>
-        </Link>
-      ))}
+      <div className='miniInfoList'>
+        {visitedData.map(data => (
+          <Link to={`/detail/${recentId}`}>
+            <MiniInfo performance={data[0]}></MiniInfo>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
