4239798a0b71dcecdba8805dc819b2c6149046b4
JZU0 <ljzh1030@gmail.com>
2023-06-18 02:27:22 +0900

    [#44] fix : 상세 페이지 링크 오류 및 UI 수정

--- src/components/RecentlyViewed/index.js
+++ src/components/RecentlyViewed/index.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { MiniInfo } from '../index'
-import './index.css'
+import { MiniInfo } from "../index";
+import "./index.css";
 
 //localStorage에서 최근 본 공연 일부 몇 개 가져오기
 
 const RecentlyViewed = () => {
-  const visitedKeys = Object.keys(localStorage).filter(key =>
-    key.includes('performanceDetail-')
-  )
-  const visitedKeysId = visitedKeys.map(key => key.split('-')[1])
-  visitedKeys.reverse()
-  console.log(visitedKeys)
-  const recentId = visitedKeysId[visitedKeysId.length - 1]
+  const visitedKeys = Object.keys(localStorage).filter((key) =>
+    key.includes("performanceDetail-")
+  );
+  const visitedKeysId = visitedKeys.map((key) => key.split("-")[1]);
+  visitedKeys.reverse();
+  const recentId = visitedKeysId[visitedKeysId.length - 1];
   let visitedData = visitedKeys.map(
-    key => JSON.parse(localStorage.getItem(key)).dbs.db
-  )
-  visitedData = visitedData.slice(Math.max(visitedData.length - 5, 0))
+    (key) => JSON.parse(localStorage.getItem(key)).dbs.db
+  );
+  visitedData = visitedData.slice(Math.max(visitedData.length - 5, 0));
   return (
-    <div className='recentlyViewed-container'>
-      <div className='rv-title'>최근 본 공연 목록</div>
-      <div className='miniInfoList'>
-        {visitedData.map(data => (
-          <Link to={`/detail/${recentId}`}>
+    <div className="recentlyViewed-container">
+      <div className="rv-title">최근 본 공연 목록</div>
+      <div className="miniInfoList">
+        {visitedData.map((data, idx) => (
+          <Link
+            to={`/detail/${data[0].mt20id}`}
+            style={{ textDecoration: "none", color: "black" }}
+            key={idx}
+          >
             <MiniInfo performance={data[0]}></MiniInfo>
           </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecentlyViewed
+export default RecentlyViewed;
